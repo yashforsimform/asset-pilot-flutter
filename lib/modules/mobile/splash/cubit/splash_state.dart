10fc7ea2cefe0bbb05db4fa8ea83c23777012c94
Rajvi Desai <rajvi.desai@simformsolutions.com>
2026-07-04 13:29:44 +0530

    Developed Splash screen.

---
+++ lib/modules/mobile/splash/cubit/splash_state.dart
@@ -0,0 +1,10 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'splash_state.freezed.dart';
+
+@freezed
+abstract class SplashState with _$SplashState {
+  const factory SplashState({
+    @Default(false) bool navigateToLogin,
+  }) = _SplashState;
+}
